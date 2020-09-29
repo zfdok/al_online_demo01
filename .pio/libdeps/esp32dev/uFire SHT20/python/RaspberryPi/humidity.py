8f20655b0f3ec080c35c1dc7c59c9d16c20a87b9
OQ <zfdok@126.com>
2020-09-29 14:28:12 +0800

    update gitignore

--- .pio/libdeps/esp32dev/uFire SHT20/python/RaspberryPi/humidity.py
+++
@@ -1,4 +0,0 @@
-from SHT20 import SHT20
-
-sht = SHT20()
-print("%.2f" %sht.humidity())
--- .pio/libdeps/esp32dev/uFire SHT20/python/RaspberryPi/temperature.py
+++
@@ -1,5 +0,0 @@
-from SHT20 import SHT20
-
-sht = SHT20()
-tempF = sht.temperature() * 1.8 + 32.0
-print("%.2f" %tempF)
